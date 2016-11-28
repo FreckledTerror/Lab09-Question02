18310e2af01608c971e257e41853217dec25c4ce
FreckledTerror <FreckledTerror97Programming@gmail.com>
2016-11-28 16:36:23 -0500

    Started working on function gradeAnswer, just need to get the final file up and running then I can start on finishing the function.

--- Lab09 Question02/Array Mastering.cpp
+++ Lab09 Question02/Array Mastering.cpp
@@ -8,35 +8,66 @@ Lab 09 Question 02
 #include<fstream>
 using namespace std;
 int gradeAnswer(string, string, int);
+float calculatePointGrade(int, int);
+string calculateLetterGrade(float);
+void readSettings(ifstream&, string&, int&);
 int main() {
 	ifstream settings;
 	string answers, key;
-	int numOfQuestions, numOfCorrectAnswers;
-	float pointGrade;
+	int numOfQuestions = 0, numOfCorrectAnswers = 0;
+	float pointGrade = 0.0;
 
 	gradeAnswer(answers, key, numOfQuestions);
 
-	calculatePointGrade(numOfCorrectAnswers, numOfQuestions);
+	//Rest of function calls are in the Word Document until needed
 
-	calculateLetterGrade(pointGrade);
-
-	readSettings(settings, key, numOfQuestions);
-
+	return 0;
 }
 
 int gradeAnswer(string answers, string key, int numOfQuestions) { 
-	
+	ifstream file, inFS;
+	string firstName, lastName;
+	char symbol;
+	float percentage;
+
+	file.open("grades.txt");
+
+	if (!file.is_open()) {
+		cout << "Could not open file grades.txt." << endl;
+	}
+
+	file >> firstName >> lastName >> percentage >> symbol >> answers;
+
+	cout << answers << endl;
+
+	inFS.open("settings.txt");
+
+	if (!inFS.is_open()) {
+		cout << "Could not open file settings.txt." << endl;
+	}
+
+	inFS >> numOfQuestions;
+	inFS >> key;
+
+	cout << numOfQuestions << endl;
+	cout << key << endl;
+
+	return 0;
 }
 
 float calculatePointGrade(int numOfCorrectAnswers, int numOfQuestions) { 
-	// To be completed 
+	
+
+	return 0;
 }
 
 string calculateLetterGrade(float pointGrade) { 
-	// To be completed 
+	
+
+	return 0;
 }
 
 
 void readSettings(ifstream& settings, string& key, int& numOfQuestions) {
-	// To be completed
+	
 } 
